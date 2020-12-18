97d29628ecb12c4673648aaacacdaf91e3998b3a
Oleg Nykolyn <juravel2@gmail.com>
2020-12-18 10:28:06 +0200

    Fix

--- amazonka-lambda/gen/Network/AWS/Lambda/Waiters.hs
+++ amazonka-lambda/gen/Network/AWS/Lambda/Waiters.hs
@@ -32,28 +32,3 @@ functionExists
            [matchStatus 200 AcceptSuccess,
             matchError "ResourceNotFoundException" AcceptRetry]}
 
--- | Polls 'Network.AWS.Lambda.GetFunctionConfiguration' every 5 seconds until a successful state is reached. An error is returned after 60 failed checks.
-functionActive :: Wait GetFunctionConfiguration
-functionActive
-  = Wait{_waitName = "FunctionActive",
-         _waitAttempts = 60, _waitDelay = 5,
-         _waitAcceptors =
-           [matchAll "Active" AcceptSuccess
-              (fcState ),
-            matchAll "Failed" AcceptFailure
-              (fcState ),
-            matchAll "Pending" AcceptRetry
-              (fcState )]}
-
--- | Polls 'Network.AWS.Lambda.GetFunctionConfiguration' every 5 seconds until a successful state is reached. An error is returned after 60 failed checks.
-functionUpdated :: Wait GetFunctionConfiguration
-functionUpdated
-  = Wait{_waitName = "FunctionUpdated",
-         _waitAttempts = 60, _waitDelay = 5,
-         _waitAcceptors =
-           [matchAll "Successful" AcceptSuccess
-              (fcLastUpdateStatus ),
-            matchAll "Failed" AcceptFailure
-              (fcLastUpdateStatus ),
-            matchAll "InProgress" AcceptRetry
-              (fcLastUpdateStatus )]}
